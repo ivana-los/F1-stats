5a52c318cb46e35aa0aeb19dd49d9b81c72c0192
233187 <ivana.nedevska@students.finki.ukim.mk>
2026-08-11 21:56:59 +0200

    index page added

--- f1/mysite/settings.py
+++ f1/mysite/settings.py
@@ -51,11 +51,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'mysite.urls'
-
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,8 +114,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
+import os
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Email
--- f1/polls/views.py
+++ f1/polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def index(request):
+    all_drivers = Driver.objects.all()
+    context = {"drivers": all_drivers, "pageTitle": "F1"}
+    return render(request, 'index.html', context)
